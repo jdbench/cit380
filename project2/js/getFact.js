a7367c05c4921538a7cb61d131d96c1f6b23cad9
jdbench <justin.d.bench@gmail.com>
2022-06-01 13:43:59 -0600

    got array to work

--- project2/js/getFact.js
+++ project2/js/getFact.js
@@ -4,28 +4,33 @@ export default function factToPage(returnElement, array) {
 
   returnElement.innerHTML = fact;
 }
-
-export async function generateFacts(json, array) {
+/*Function that pulls data from the JSON and returns it to an array*/
+export async function generateFacts(json) {
   let response = await fetch(json);
   let data = await response.json();
-  let randomSeason = Math.floor(Math.random() * data.survivor.seasons.length);
-  console.log(data.survivor.seasons[randomSeason]);
 
-  return array;
+  return data;
 }
-
+/*Function that gets the array and iterates through the array
+with random indexes*/
 export function getFact(array) {
+  let facts = [];
   let fact;
-  let randomIndex;
-
+  let randomSurvivor;
+  let randomSeason;
+  let season;
+  let survivorData;
+/*Try catch block for debugging */
   try {
-    if (array.length == 0 || array.length == undefined) {
+/*If there is no data in the array, return a string. Else,
+get a random season, get random survivor data. */
+    if (array.length == 0) {
       fact = "There is no Survivor data here yet, please come back later";
     } else {
-      randomIndex = Math.floor(Math.random() * array.length);
-      fact = array[randomIndex];
+      randomSeason = Math.floor(Math.random() * array.length);
+      console.log(array[randomSeason]);
     }
-  } catch (error){
+  } catch (error) {
     fact = "There was an error: " + error;
   }
 
--- project2/js/project2.js
+++ project2/js/project2.js
@@ -1,9 +1,12 @@
 import factToPage, { generateFacts } from "./getFact.js";
 let factArray = [];
-const json = "./js/test.json";
+const json = "./js/sample.json";
 const factOnPage = document.getElementById("fact");
 
-factArray = generateFacts(json, factArray);
+generateFacts(json).then((data) => {
+  console.log(data);
+  factArray = data;
+});
 
 document.getElementById("get-fact").addEventListener("click", function () {
   factToPage(factOnPage, factArray);
